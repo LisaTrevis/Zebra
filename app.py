d50649ab70bd4db30a1704be722cb3a66443c5a9
Lisa Trevis <l_trevis@hotmail.com>
2019-07-23 10:29:43 -0700

    print statements

--- app.py
+++ app.py
@@ -1,1 +1,33 @@
 print("Hello World")
+
+print("   /|")
+print("  / |")
+print(" /  |")
+print("/___|")
+
+character_name = "Lisa"
+character_age = "42"
+
+print("There once was a woman named " + character_name + ".")
+print("She was " + character_age + " years old.")
+
+print("Zebra\n\"Academy\"")
+
+school = "Zebra\Academy"
+print(school)
+
+print(school.lower())
+
+print(school.upper())
+
+print(school.isupper())
+
+print(school.upper().isupper())
+
+print(len(school))
+
+print(school[6])
+
+print(school.index("a"))
+
+print(school.replace("Zebra", "Peacock"))
