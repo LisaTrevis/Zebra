ec3055bbe0b95605db6432a4d3664013fd632120
Lisa Trevis <l_trevis@hotmail.com>
2019-09-18 12:22:14 -0700

    added more examples

--- app.py
+++ app.py
@@ -31,3 +31,37 @@ print(school[6])
 print(school.index("a"))
 
 print(school.replace("Zebra", "Peacock"))
+
+# Numbers
+print(20)
+
+print(-230)
+
+print(3+4.5)
+
+print((3+4) * 2)
+
+print(10 % 3)
+
+my_num = 13
+print(str(my_num) + " is my fave number.")
+
+my_num = -5
+print(abs(my_num))
+
+# To set a number to a power
+print(pow(3, 2))
+
+print(max(3, 7))
+print(min(3, 7))
+
+print(round(3.2))
+print(round(3.7))
+
+# importing stuff
+from math import *
+
+print(floor(4.7))
+print(ceil(5.8))
+print(sqrt(49))
+
